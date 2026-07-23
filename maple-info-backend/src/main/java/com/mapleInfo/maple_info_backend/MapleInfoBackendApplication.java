0659249e6a9a7dc6b44c20211c7556bdbe479363
zmxn9941@naver.com <zmxn9941@naver.com>
2026-07-23 12:43:32 +0900

    first commit

---
+++ maple-info-backend/src/main/java/com/mapleInfo/maple_info_backend/MapleInfoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mapleInfo.maple_info_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleInfoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapleInfoBackendApplication.class, args);
+	}
+
+}
---
+++ maple-info-backend/src/test/java/com/mapleInfo/maple_info_backend/MapleInfoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapleInfo.maple_info_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleInfoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
