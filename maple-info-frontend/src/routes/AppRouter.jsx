4569fb30a066734c9371fd2c42cd254be52d45dd
Navii112 <hyun010902@gmail.com>
2026-07-29 12:40:40 +0900

    fix : 폰트 수정 헤더 추가 완료

--- maple-info-frontend/src/routes/AppRouter.jsx
+++ maple-info-frontend/src/routes/AppRouter.jsx
@@ -4,39 +4,21 @@ import MainLayout from "../layouts/MainLayout";
 import CharacterPage from "../pages/character/CharacterPage.jsx";
 import CubeExpectation from "../components/cubeCalculator/CubeExpectation";
 import StarforceExpectation from "../components/starforceCalculator/StarforceExpectation";
-import DeviceFrame from "../components/device/DeviceFrame";
+// 🌟 밖으로 빼두었던 Header와 DeviceFrame import는 지워도 됩니다. (MainLayout이 알아서 처리해줍니다)
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
+          
           <Route index element={<HomePage />} />
           <Route path="character/:characterName" element={<CharacterPage />} />
+          
+          <Route path="cube" element={<CubeExpectation />} />
+          <Route path="starforce" element={<StarforceExpectation />} />
+          
         </Route>
-
-          <Route
-              path="character/:characterName"
-              element={<CharacterPage />}
-          />
-
-        <Route
-          path="/cube"
-          element={
-            <DeviceFrame>
-              <CubeExpectation />
-            </DeviceFrame>
-          }
-        />
-
-        <Route
-          path="/starforce"
-          element={
-            <DeviceFrame>
-              <StarforceExpectation />
-            </DeviceFrame>
-          }
-        />
       </Routes>
     </BrowserRouter>
   );
